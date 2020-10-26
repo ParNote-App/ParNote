11aea7e985645bd058fc7f884354ecc2ba983297
kahverengi <ahmet.duruer@yandex.com>
2020-10-26 17:37:55 +0100

    Update LoginAPI for new API abstract class methods

--- src/main/kotlin/com/parnote/route/api/LoginAPI.kt
+++ src/main/kotlin/com/parnote/route/api/LoginAPI.kt
@@ -1,20 +1,13 @@
 package com.parnote.route.api
 
 import com.parnote.model.Api
-import com.parnote.model.RouteType
-import io.vertx.core.Handler
+import com.parnote.model.Result
 import io.vertx.ext.web.RoutingContext
 
-
 class LoginAPI: Api() {
-
-
     override val routes: ArrayList<String> = arrayListOf("/api/testAPI", "/alp/merhaba")
 
-
-    override fun getHandler() = Handler<RoutingContext>  {
-        it.response().end("Hello from API.")
+    override fun getHandler(context: RoutingContext, handler: (result: Result) -> Unit) {
+        context.response().end("Hello from API.")
     }
-
-
 }

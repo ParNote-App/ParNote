3f952d9ef3f35cdde48a908ba7945a30a27072ca
TheNavyInfantry <nedimcanulusoy@gmail.com>
2020-10-27 23:16:51 +0300

    Add isLoginCorrect to UserDao

--- src/main/kotlin/com/parnote/db/entity/UserDaoImpl.kt
+++ src/main/kotlin/com/parnote/db/entity/UserDaoImpl.kt
@@ -5,13 +5,14 @@ import com.parnote.db.dao.UserDao
 import io.vertx.core.AsyncResult
 import io.vertx.core.json.JsonArray
 import io.vertx.ext.sql.SQLConnection
+import org.apache.commons.codec.digest.DigestUtils
 
 class UserDaoImpl(override val tableName: String = "user") : DaoImpl(), UserDao {
 
     override fun init(): (sqlConnection: SQLConnection, handler: (asyncResult: AsyncResult<*>) -> Unit) -> SQLConnection =
-        { sqlConnection, handler ->
-            sqlConnection.query(
-                """
+            { sqlConnection, handler ->
+                sqlConnection.query(
+                        """
             CREATE TABLE IF NOT EXISTS `${getTablePrefix() + tableName}` (
               `id` int NOT NULL AUTO_INCREMENT,
               `username` varchar(16) NOT NULL UNIQUE,
@@ -26,15 +27,15 @@ class UserDaoImpl(override val tableName: String = "user") : DaoImpl(), UserDao 
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='User Table';
         """
-            ) {
-                handler.invoke(it)
+                ) {
+                    handler.invoke(it)
+                }
             }
-        }
 
     override fun isEmailExists(
-        email: String,
-        sqlConnection: SQLConnection,
-        handler: (exists: Boolean?, asyncResult: AsyncResult<*>) -> Unit
+            email: String,
+            sqlConnection: SQLConnection,
+            handler: (exists: Boolean?, asyncResult: AsyncResult<*>) -> Unit
     ) {
         val query =
                 "SELECT COUNT(email) FROM `${getTablePrefix() + tableName}` where email = ?"
@@ -56,6 +57,22 @@ class UserDaoImpl(override val tableName: String = "user") : DaoImpl(), UserDao 
                 "SELECT COUNT(username) FROM `${getTablePrefix() + tableName}` where username = ?"
 
         sqlConnection.queryWithParams(query, JsonArray().add(username)) { queryResult ->
+            if (queryResult.succeeded())
+                handler.invoke(queryResult.result().results[0].getInteger(0) == 1, queryResult)
+            else
+                handler.invoke(null, queryResult)
+        }
+    }
+
+    override fun isLoginCorrect(
+            usernameOrEmail: String,
+            password: String,
+            sqlConnection: SQLConnection, handler: (loginCorrect: Boolean?, asyncResult: AsyncResult<*>) -> Unit) {
+
+        val query =
+                "SELECT COUNT(`id`) FROM `${getTablePrefix() + tableName}` where (username = ? or email = ?) and password = ?"
+
+        sqlConnection.queryWithParams(query, JsonArray().add(usernameOrEmail).add(usernameOrEmail).add(DigestUtils.md5Hex(password))) { queryResult ->
             if (queryResult.succeeded())
                 handler.invoke(queryResult.result().results[0].getInteger(0) == 1, queryResult)
             else

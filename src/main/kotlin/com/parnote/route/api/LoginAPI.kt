9f18a0329686a499036cebd6ea1d5ce216f5bd17
alphazarc <alphazarc@gmail.com>
2020-10-27 22:40:25 +0300

    Add form validation

--- src/main/kotlin/com/parnote/route/api/LoginAPI.kt
+++ src/main/kotlin/com/parnote/route/api/LoginAPI.kt
@@ -1,13 +1,67 @@
 package com.parnote.route.api
 
-import com.parnote.model.Api
-import com.parnote.model.Result
+import com.parnote.ErrorCode
+import com.parnote.Main.Companion.getComponent
+import com.parnote.db.DatabaseManager
+import com.parnote.model.*
+import de.triology.recaptchav2java.ReCaptcha
 import io.vertx.ext.web.RoutingContext
+import javax.inject.Inject
+import kotlin.Error
+
 
 class LoginAPI: Api() {
-    override val routes: ArrayList<String> = arrayListOf("/api/testAPI", "/alp/merhaba")
+    override val routes: ArrayList<String> = arrayListOf("/api/auth/loginAPI")
+
+    override val routeType = RouteType.POST
+
+
+    init {
+        getComponent().inject(this)
+    }
+
+    @Inject
+    lateinit var databaseManager: DatabaseManager
+
+    @Inject
+    lateinit var reCaptcha: ReCaptcha
+
+
 
     override fun getHandler(context: RoutingContext, handler: (result: Result) -> Unit) {
-        context.response().end("Hello from API.")
+        val data = context.bodyAsJson
+
+        val usernameOrEmail = data.getString("usernameOrEmail")
+        val password = data.getString("password")
+        val rememberMe = data.getBoolean("rememberMe")
+
+        validateForm(usernameOrEmail,password, handler) {
+
+        }
+
+        handler.invoke(Successful())
+
+        handler.invoke(Error(ErrorCode.UNKNOWN_ERROR))
+    }
+
+    fun validateForm(usernameOrEmail: String, password: String, errorHandler: (result: Result) -> Unit, successHandler: () -> Unit){
+
+        if (usernameOrEmail.isEmpty()) {
+            errorHandler.invoke(Error(ErrorCode.LOGIN_USERNAME_OR_EMAIL_INVALID))
+            return
+        }
+
+        if (!usernameOrEmail.matches(Regex("^[a-zA-Z0-9]+\$")) && !usernameOrEmail.matches(Regex("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}\$"))) {
+            errorHandler.invoke(Error(ErrorCode.LOGIN_USERNAME_OR_EMAIL_INVALID))
+            return
+        }
+
+        if (password.isEmpty()){
+            errorHandler.invoke(Error(ErrorCode.LOGIN_PASSWORD_INVALID))
+        }
+
+        successHandler.invoke()
+
+
     }
 }

5cab6df770d5d25a3481a948ddd64125fe7ba869
kahverengi <ahmet.duruer@yandex.com>
2020-10-29 21:04:11 +0100

    Add getSecretKeyByID method to UserDao

--- src/main/kotlin/com/parnote/db/entity/UserDaoImpl.kt
+++ src/main/kotlin/com/parnote/db/entity/UserDaoImpl.kt
@@ -2,10 +2,16 @@ package com.parnote.db.entity
 
 import com.parnote.db.DaoImpl
 import com.parnote.db.dao.UserDao
+import com.parnote.db.model.User
+import com.parnote.model.Result
+import com.parnote.model.Successful
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
 import io.vertx.core.AsyncResult
 import io.vertx.core.json.JsonArray
 import io.vertx.ext.sql.SQLConnection
 import org.apache.commons.codec.digest.DigestUtils
+import java.util.*
 
 class UserDaoImpl(override val tableName: String = "user") : DaoImpl(), UserDao {
 
@@ -70,9 +76,12 @@ class UserDaoImpl(override val tableName: String = "user") : DaoImpl(), UserDao 
             sqlConnection: SQLConnection, handler: (loginCorrect: Boolean?, asyncResult: AsyncResult<*>) -> Unit) {
 
         val query =
-                "SELECT COUNT(`id`) FROM `${getTablePrefix() + tableName}` where (username = ? or email = ?) and password = ?"
+            "SELECT COUNT(`id`) FROM `${getTablePrefix() + tableName}` where (username = ? or email = ?) and password = ?"
 
-        sqlConnection.queryWithParams(query, JsonArray().add(usernameOrEmail).add(usernameOrEmail).add(DigestUtils.md5Hex(password))) { queryResult ->
+        sqlConnection.queryWithParams(
+            query,
+            JsonArray().add(usernameOrEmail).add(usernameOrEmail).add(DigestUtils.md5Hex(password))
+        ) { queryResult ->
             if (queryResult.succeeded())
                 handler.invoke(queryResult.result().results[0].getInteger(0) == 1, queryResult)
             else
@@ -80,4 +89,49 @@ class UserDaoImpl(override val tableName: String = "user") : DaoImpl(), UserDao 
         }
     }
 
+    override fun add(
+        user: User,
+        sqlConnection: SQLConnection,
+        handler: (isSuccessful: Result?, asyncResult: AsyncResult<*>) -> Unit
+    ) {
+        val query =
+            "INSERT INTO `${getTablePrefix() + tableName}` (username, email, password, permission_id, registered_ip, secret_key, public_key, register_date) " +
+                    " VALUES (?, ?, ?, ?, ?, ?, ?, ?); "
+
+        val key = Keys.keyPairFor(SignatureAlgorithm.RS256)
+
+        sqlConnection.queryWithParams(
+            query,
+            JsonArray()
+                .add(user.username)
+                .add(user.email)
+                .add(user.password)
+                .add(user.permissionID)
+                .add(user.ipAddress)
+                .add(Base64.getEncoder().encodeToString(key.private.encoded))
+                .add(Base64.getEncoder().encodeToString(key.public.encoded))
+                .add(System.currentTimeMillis())
+        ) { queryResult ->
+            if (queryResult.succeeded())
+                handler.invoke(Successful(), queryResult)
+            else
+                handler.invoke(null, queryResult)
+        }
+    }
+
+    override fun getSecretKeyByID(
+        userID: Int,
+        sqlConnection: SQLConnection,
+        handler: (result: String?, asyncResult: AsyncResult<*>) -> Unit
+    ) {
+        val query =
+            "SELECT secret_key FROM `${databaseManager.getTablePrefix() + tableName}` where `id` = ?"
+
+        sqlConnection.queryWithParams(query, JsonArray().add(userID)) { queryResult ->
+            if (queryResult.succeeded())
+                handler.invoke(queryResult.result().results[0].getString(0), queryResult)
+            else
+                handler.invoke(null, queryResult)
+        }
+    }
 }

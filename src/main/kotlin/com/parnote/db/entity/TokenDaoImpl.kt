4825827c51df8e6a75670196baa1295ebfa6bc39
kahverengi <ahmet.duruer@yandex.com>
2020-10-29 21:35:27 +0100

    Add insert method to TokenDao

--- src/main/kotlin/com/parnote/db/entity/TokenDaoImpl.kt
+++ src/main/kotlin/com/parnote/db/entity/TokenDaoImpl.kt
@@ -2,7 +2,11 @@ package com.parnote.db.entity
 
 import com.parnote.db.DaoImpl
 import com.parnote.db.dao.TokenDao
+import com.parnote.db.model.Token
+import com.parnote.model.Result
+import com.parnote.model.Successful
 import io.vertx.core.AsyncResult
+import io.vertx.core.json.JsonArray
 import io.vertx.ext.sql.SQLConnection
 
 class TokenDaoImpl(override val tableName: String = "token") : DaoImpl(), TokenDao {
@@ -23,4 +27,26 @@ class TokenDaoImpl(override val tableName: String = "token") : DaoImpl(), TokenD
                 handler.invoke(it)
             }
         }
+
+    override fun add(
+        token: Token,
+        sqlConnection: SQLConnection,
+        handler: (result: Result?, asyncResult: AsyncResult<*>) -> Unit
+    ) {
+        sqlConnection.updateWithParams(
+            """
+                INSERT INTO `${databaseManager.getTablePrefix() + tableName}` (token, created_time, user_id, subject) VALUES (?, ?, ?, ?)
+            """.trimIndent(),
+            JsonArray()
+                .add(token.token)
+                .add(System.currentTimeMillis())
+                .add(token.userID)
+                .add(token.subject)
+        ) {
+            if (it.succeeded())
+                handler.invoke(Successful(), it)
+            else
+                handler.invoke(null, it)
+        }
+    }
 }

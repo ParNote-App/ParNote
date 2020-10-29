a4e89603324a7b7e32cea78ba3cdf5e341d46cb7
kahverengi <ahmet.duruer@yandex.com>
2020-10-29 21:04:11 +0100

    Add getSecretKeyByID method to UserDao

--- src/main/kotlin/com/parnote/db/dao/UserDao.kt
+++ src/main/kotlin/com/parnote/db/dao/UserDao.kt
@@ -2,6 +2,7 @@ package com.parnote.db.dao
 
 import com.parnote.db.Dao
 import com.parnote.db.model.User
+import com.parnote.model.Result
 import io.vertx.core.AsyncResult
 import io.vertx.ext.sql.SQLConnection
 
@@ -14,15 +15,27 @@ interface UserDao : Dao<User> {
     )
 
     fun isUsernameExists(
-            username: String,
-            sqlConnection: SQLConnection,
-            handler: (exists: Boolean?, asyncResult: AsyncResult<*>) -> Unit
+        username: String,
+        sqlConnection: SQLConnection,
+        handler: (exists: Boolean?, asyncResult: AsyncResult<*>) -> Unit
     )
 
     fun isLoginCorrect(
-            usernameOrEmail: String,
-            password: String,
-            sqlConnection: SQLConnection,
-            handler: (loginCorrect: Boolean?, asyncResult: AsyncResult<*>) -> Unit
+        usernameOrEmail: String,
+        password: String,
+        sqlConnection: SQLConnection,
+        handler: (loginCorrect: Boolean?, asyncResult: AsyncResult<*>) -> Unit
+    )
+
+    fun add(
+        user: User,
+        sqlConnection: SQLConnection,
+        handler: (isSuccessful: Result?, asyncResult: AsyncResult<*>) -> Unit
+    )
+
+    fun getSecretKeyByID(
+        userID: Int,
+        sqlConnection: SQLConnection,
+        handler: (result: String?, asyncResult: AsyncResult<*>) -> Unit
     )
 }

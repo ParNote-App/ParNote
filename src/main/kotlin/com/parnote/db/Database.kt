a9504b0bc7c32e1480e5e2d3ab5cda5d55bfe639
kahverengi <ahmet.duruer@yandex.com>
2020-10-29 20:31:48 +0100

    Add TokenDao

--- src/main/kotlin/com/parnote/db/Database.kt
+++ src/main/kotlin/com/parnote/db/Database.kt
@@ -1,16 +1,20 @@
 package com.parnote.db
 
 import com.parnote.db.dao.SchemeVersionDao
+import com.parnote.db.dao.TokenDao
 import com.parnote.db.dao.UserDao
 import com.parnote.db.entity.SchemeVersionDaoImpl
+import com.parnote.db.entity.TokenDaoImpl
 import com.parnote.db.entity.UserDaoImpl
 
 data class Database(
     val schemeVersionDao: SchemeVersionDao = SchemeVersionDaoImpl(),
-    val userDao: UserDao = UserDaoImpl()
+    val userDao: UserDao = UserDaoImpl(),
+    val tokenDao: TokenDao = TokenDaoImpl()
 ) {
     fun init() = listOf(
         schemeVersionDao.init(),
-        userDao.init()
+        userDao.init(),
+        tokenDao.init()
     )
 }

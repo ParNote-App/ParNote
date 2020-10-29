9be9a48f5a64d146e4e8ce6bb2ac7a6026445c64
kahverengi <ahmet.duruer@yandex.com>
2020-10-29 21:35:27 +0100

    Add insert method to TokenDao

--- src/main/kotlin/com/parnote/db/dao/TokenDao.kt
+++ src/main/kotlin/com/parnote/db/dao/TokenDao.kt
@@ -2,7 +2,11 @@ package com.parnote.db.dao
 
 import com.parnote.db.Dao
 import com.parnote.db.model.Token
+import com.parnote.model.Result
+import io.vertx.core.AsyncResult
+import io.vertx.ext.sql.SQLConnection
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
 interface TokenDao : Dao<Token> {
+    fun add(token: Token, sqlConnection: SQLConnection, handler: (result: Result?, asyncResult: AsyncResult<*>) -> Unit)
 }

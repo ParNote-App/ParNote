c2a1c4719e24721cb576f41f1188d0b012331ba6
archely <arda_atila@hotmail.com>
2020-10-30 18:27:42 +0100

    Add ResetPasswordAPI to router module

--- src/main/kotlin/com/parnote/di/module/RouterModule.kt
+++ src/main/kotlin/com/parnote/di/module/RouterModule.kt
@@ -4,6 +4,7 @@ import com.parnote.model.Api
 import com.parnote.model.RouteType
 import com.parnote.model.Template
 import com.parnote.route.api.LoginAPI
+import com.parnote.route.api.ResetPasswordAPI
 import com.parnote.route.staticFolder.PublicFolder
 import com.parnote.route.template.IndexTemplate
 import dagger.Module
@@ -42,7 +43,8 @@ class RouterModule(private val mVertx: Vertx) {
 
     private val mAPIRouteList by lazy {
         arrayOf<Api>(
-            LoginAPI()
+            LoginAPI(),
+            ResetPasswordAPI()
         )
     }
 

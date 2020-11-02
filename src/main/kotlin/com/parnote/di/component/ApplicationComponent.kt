877e6d07bf21bfda3ef97676f961db6ac0374411
archely <arda_atila@hotmail.com>
2020-11-02 19:54:43 +0100

    Add token check, mark user email verified to EmailVerificationAPI

--- src/main/kotlin/com/parnote/di/component/ApplicationComponent.kt
+++ src/main/kotlin/com/parnote/di/component/ApplicationComponent.kt
@@ -4,6 +4,7 @@ import com.parnote.Main
 import com.parnote.db.DaoImpl
 import com.parnote.db.DatabaseMigration
 import com.parnote.di.module.*
+import com.parnote.route.api.EmailVerificationAPI
 import com.parnote.route.api.RegisterAPI
 import com.parnote.route.api.ResetPasswordAPI
 import dagger.Component
@@ -30,4 +31,6 @@ interface ApplicationComponent {
     fun inject(registerAPI: RegisterAPI)
 
     fun inject(resetPasswordAPI: ResetPasswordAPI)
+
+    fun inject(emailVerificationAPI: EmailVerificationAPI)
 }

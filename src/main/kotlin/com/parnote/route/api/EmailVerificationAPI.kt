829ddef66431944a6fa777f26dfbe17f1c6b207a
kahverengi <ahmet.duruer@yandex.com>
2020-11-05 20:11:11 +0100

    Review EmailVerificationAPI

--- src/main/kotlin/com/parnote/route/api/EmailVerificationAPI.kt
+++ src/main/kotlin/com/parnote/route/api/EmailVerificationAPI.kt
@@ -8,11 +8,9 @@ import com.parnote.model.*
 import io.vertx.ext.web.RoutingContext
 import javax.inject.Inject
 
-@Suppress("UNREACHABLE_CODE")
 class EmailVerificationAPI : Api() {
+    override val routes = arrayListOf("/api/auth/EmailVerificationAPI")
 
-
-    override val routes = arrayListOf("Api/auth/EmailVerificationAPI")
     override val routeType = RouteType.POST
 
     init {
@@ -22,9 +20,7 @@ class EmailVerificationAPI : Api() {
     @Inject
     lateinit var databaseManager: DatabaseManager
 
-
     override fun getHandler(context: RoutingContext, handler: (result: Result) -> Unit) {
-
         val data = context.bodyAsJson
 
         val token = data.getString("token")
@@ -32,6 +28,7 @@ class EmailVerificationAPI : Api() {
         databaseManager.createConnection { sqlConnection, _ ->
             if (sqlConnection == null) {
                 handler.invoke(Error(ErrorCode.UNKNOWN_ERROR))
+
                 return@createConnection
             }
 
@@ -40,46 +37,40 @@ class EmailVerificationAPI : Api() {
                     databaseManager.closeConnection(sqlConnection) {
                         handler.invoke(Error(ErrorCode.UNKNOWN_ERROR_3))
                     }
+
                     return@isTokenExists
                 }
 
                 if (!exists) {
                     databaseManager.closeConnection(sqlConnection) {
                         handler.invoke(Error(ErrorCode.EMAIL_VERIFICATION_INVALID_TOKEN))
-
                     }
+
                     return@isTokenExists
                 }
-
 
                 databaseManager.getDatabase().tokenDao.getUserIDFromToken(token, sqlConnection) { userID, _ ->
                     if (userID == null) {
                         databaseManager.closeConnection(sqlConnection) {
                             handler.invoke(Error(ErrorCode.UNKNOWN_ERROR_4))
                         }
+
                         return@getUserIDFromToken
                     }
 
                     databaseManager.getDatabase().userDao.makeEmailVerifiedByID(userID, sqlConnection) { result, _ ->
-                        if (result == null) {
-                            databaseManager.closeConnection(sqlConnection) {
-                                handler.invoke(Error(ErrorCode.UNKNOWN_ERROR_4))
-                            }
-                            return@makeEmailVerifiedByID
-                        }
                         databaseManager.closeConnection(sqlConnection) {
+                            if (result == null) {
+                                handler.invoke(Error(ErrorCode.UNKNOWN_ERROR_4))
+
+                                return@closeConnection
+                            }
+
                             handler.invoke(Successful())
                         }
-
-
                     }
-
                 }
-
-
             }
-
-
         }
     }
 }

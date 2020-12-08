087b14f5b84f2e503635530865d9f578e8f7a0a6
kahverengi <ahmet.duruer@yandex.com>
2020-12-08 22:51:03 +0300

    Add dev builds on Travis-CI

---
+++ travis-scripts/install_script.sh
@@ -0,0 +1,15 @@
+nvm use 11.0.0
+npm --version
+node --version
+./gradlew build
+git clone https://github.com/ParNote-App/ParNote-UI.git parnote-ui
+cd parnote-ui
+npm install
+npm run build
+ls -a
+ls ..
+cp -avr public ../public
+cd ..
+rm -rf parnote-ui
+ls -a
+ls -a public

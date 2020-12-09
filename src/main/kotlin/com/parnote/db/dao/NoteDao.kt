a883f809d528ae9ec424e04034c8702a6baca424
kahverengi <ahmet.duruer@yandex.com>
2020-12-09 23:35:05 +0300

    Add add method to NoteDao

--- src/main/kotlin/com/parnote/db/dao/NoteDao.kt
+++ src/main/kotlin/com/parnote/db/dao/NoteDao.kt
@@ -2,6 +2,7 @@ package com.parnote.db.dao
 
 import com.parnote.db.Dao
 import com.parnote.db.model.Note
+import com.parnote.model.Result
 import io.vertx.core.AsyncResult
 import io.vertx.ext.sql.SQLConnection
 
@@ -11,5 +12,11 @@ interface NoteDao : Dao<Note> {
         userID: Int,
         sqlConnection: SQLConnection,
         handler: (notes: List<Map<String, Any>>?, asyncResult: AsyncResult<*>) -> Unit
+    )
+
+    fun add(
+        note: Note,
+        sqlConnection: SQLConnection,
+        handler: (result: Result?, asyncResult: AsyncResult<*>) -> Unit
     )
 }
--- src/main/kotlin/com/parnote/db/entity/NoteDaoImpl.kt
+++ src/main/kotlin/com/parnote/db/entity/NoteDaoImpl.kt
@@ -2,6 +2,9 @@ package com.parnote.db.entity
 
 import com.parnote.db.DaoImpl
 import com.parnote.db.dao.NoteDao
+import com.parnote.db.model.Note
+import com.parnote.model.Result
+import com.parnote.model.Successful
 import io.vertx.core.AsyncResult
 import io.vertx.core.json.JsonArray
 import io.vertx.ext.sql.SQLConnection
@@ -61,6 +64,29 @@ class NoteDaoImpl(override val tableName: String = "note") : DaoImpl(), NoteDao 
                 handler.invoke(notes, queryResult)
             } else
                 handler.invoke(null, queryResult)
+        }
+    }
+
+    override fun add(
+        note: Note,
+        sqlConnection: SQLConnection,
+        handler: (result: Result?, asyncResult: AsyncResult<*>) -> Unit
+    ) {
+        sqlConnection.updateWithParams(
+            """
+                INSERT INTO `${getTablePrefix() + tableName}` (user_id, title, text, last_modified, status) VALUES (?, ?, ?, ?, ?)
+            """.trimIndent(),
+            JsonArray()
+                .add(note.userID)
+                .add(Base64.getEncoder().encodeToString(note.title.toByteArray()))
+                .add(Base64.getEncoder().encodeToString(note.text.toByteArray()))
+                .add(System.currentTimeMillis())
+                .add(1)
+        ) {
+            if (it.succeeded())
+                handler.invoke(Successful(), it)
+            else
+                handler.invoke(null, it)
         }
     }
 }
